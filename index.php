2ed54a26fa6ca30f58077dbe52d53590ef650311
Kona <Kona@Koodaaja.fi>
2025-10-06 16:15:41 +0300

    Edelleenlähetyksen pohja

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
+<?php
+$osoitteet = array(
+    "NG5TG" => "https://www.w3schools.com/php/",
+    "R7E7L" => "https://www.php.net/manual/en/index.php",
+    "S44E8" => "https://thevalleyofcode.com/php/",
+    "UDCJ9" => "https://phpapprentice.com/",
+    "ZZU1M" => "https://phptherightway.com/",
+    "FOOOO" => "https://www.example.com/"
+);
+
+if (isset($_GET["hash"])) {
+    $hash = $_GET["hash"];
+    if (isset($osoitteet[$hash])) {
+        header("Location: " . $osoitteet[$hash]);
+        exit;
+    } else {
+        echo "Väärä tunniste :(";
+    }
+} else {
+    echo "Tämä on osoitteiden lyhentäjä. Odota maltilla, tänne tulee tulevaisuudessa lisää toiminnallisuutta.";
+}
+?>
 
